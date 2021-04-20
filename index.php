77997e74e91b312e22683c4da315549ae1562abb
Wei-Hao Wang <wwang03@risd.edu>
2021-04-20 21:15:59 +0800

    first cleanup

--- index.php
+++ index.php
@@ -2,10 +2,12 @@
 $uri = explode('/', $_SERVER['REQUEST_URI']);
 
 require_once("views/head.php");
-if ($uri[1] == 'print')
-    require_once("views/print.php");
-else if ($uri[1] == 'read')
-    require_once("views/read.php");
+if (!$uri[1])
+	require_once("views/home.php");
+elseif ($uri[1] == 'collection' && count($uri < 4))
+	require_once("views/list.php");
+elseif ($uri[1] == 'collection' && count($uri >= 4))
+	require_once("views/detail.php");
 else 
     require_once("views/main.php");
 require_once("views/badge.php");
