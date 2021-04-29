97eede80d38502ddeb983f78f9d65c80a5dbe1af
Wei-Hao Wang <wwang03@risd.edu>
2021-04-29 23:42:08 +0800

    debug

--- views/season-lobby.php
+++ views/season-lobby.php
@@ -14,6 +14,7 @@
 		$children_count = count($children) > 10 ? count($children) : '0' . count($children);
 		foreach($children as $key => $child)
 		{
+			var_dump($child['id']);
 			$media = $oo->media($child['id']);
 			var_dump($media);
 			$thumbnail = false;
