84aff46ecad30fc426279e127c5ba43c1f310a8c
Wei-Hao Wang <wwang03@risd.edu>
2021-05-05 22:00:16 +0800

    adjust homepage animation

--- views/home.php
+++ views/home.php
@@ -1,5 +1,5 @@
 <?
-	$blue_count = 7;
+	$blue_count = 8;
 	$white_count = 2;
 	$group_count = 6;
 	$size = array();
@@ -14,23 +14,23 @@
 					<?
 					for($j = 0; $j < $blue_count; $j++)
 					{
-						$w = rand(20, 200);
-						$h = rand(20, 200);
+						$w = rand(6, 18);
+						$h = rand(6, 18);
 						$left = rand(0, 100);
 						$top = rand(0, 100);
 						$size[] = array('w' => $w, 'h' => $h);
-						$style="width:".$w."px;height:".$h."px;top:".$top."vh;left:".$left."vw;"; 
+						$style="width:".$w."%;height:".$h."%;top:".$top."vh;left:".$left."vw;"; 
 
 						?><div class="sky-element sky-blue" style="<?= $style; ?>"></div><?
 					}
 					for($j = 0; $j < $white_count; $j++)
 					{
-						$w = rand(60, 200);
-						$h = rand(60, 200);
+						$w = rand(5, 18);
+						$h = rand(5, 18);
 						$left = rand(0, 100);
 						$top = rand(0, 100);
 						$size[] = array('w' => $w, 'h' => $h);
-						$style="width:".$w."px;height:".$h."px;top:".$top."vh;left:".$left."vw;"; 
+						$style="width:".$w."%;height:".$h."%;top:".$top."vh;left:".$left."vw;"; 
 
 						?><div class="sky-element sky-white" style="<?= $style; ?>"></div><?
 					}
@@ -53,61 +53,71 @@
 	var sSky_group = document.getElementsByClassName('sky-group');
 	var direction;
 	var isFloating = false;
-	var float_min = 5;
-	var float_max = 10;
+	var float_min = 10;
+	var float_max = 15;
 	var float_maxmin = float_max - float_min;
-	var speed = 10;
+	var speed = 8;
+	var sky_timer;
 	
 	[].forEach.call(sSky_group, function(el, i){
-		el.style.transform = 'translate3d(5px, 7px, 0)';
+		el.style.transform = 'translate3d(0, 0, 0)';
 	});
 	
 	sSky_container.addEventListener('mouseover', function(){
 		if(!isFloating)
 		{
 			isFloating = true;
-			[].forEach.call(sSky_group, function(el, i){
-				direction = getRandomInt(0, 3);
-				duration = parseInt(10*(float_maxmin * Math.random())) / 10 + float_min;
-				el.style.transition = 'transform '+duration+'s';
-				var style = window.getComputedStyle(el);
-				var matrix = new WebKitCSSMatrix(style.transform);
-				
-				var x = matrix.m41;
-				var y = matrix.m42;
-				// console.log(x, y);
-				var distance = duration * speed;
-				// console.log(distance);
-				
-				if(direction == 0){
-					var this_transform = 'translate3d('+x+'px,'+(y+distance)+'px,0)';
-				}
-				else if(direction == 1){
-					var this_transform = 'translate3d('+(x+distance)+'px,'+y+'px,0)';
-				}
-				else if(direction == 2){
-					var this_transform = 'translate3d('+x+'px,'+(y-distance)+'px,0)';
-				}
-				else if(direction == 3){
-					var this_transform = 'translate3d('+(x-distance)+'px,'+y+'px,0)';
-				}
-			 	el.style.transform = this_transform;
-			});
-			setTimeout(()=>isFloating = false, 5000);
+			
 		}
 	});
-	// [].forEach.call(sSky_blue, function(el, i){
-	// 	 direction = getRandomInt(0, 3)
-	// 	 if(direction == 0)
-	// 	 	el.style.transform = parseInt(el.style.top) + 1 + 'px';
-	// 	 else if(direction == 1)
-	// 	 	el.style.left = parseInt(el.style.left) + 1 + 'px';
-	// 	 else if(direction == 2)
-	// 	 	el.style.top = parseInt(el.style.top) - 1 + 'px';
-	// 	 else if(direction == 3)
-	// 	 	el.style.left = parseInt(el.style.left) - 1 + 'px';
-	// });
-	var style = window.getComputedStyle(sSky_blue[0]);
-	var matrix = new WebKitCSSMatrix(style.transform);
-	console.log(matrix);
+
+	
+	wind(sSky_group, 5000);
+
+	function wind(groups, duration){
+		[].forEach.call(groups, function(el, i){
+			var direction = getRandomInt(0, 3);
+			var transition = parseInt(duration / 100)/10;
+			el.style.transition = 'transform '+transition+'s';
+			var style = window.getComputedStyle(el);
+			var matrix = new WebKitCSSMatrix(style.transform);
+			
+			var x = matrix.m41;
+			var y = matrix.m42;
+			var distance = transition * speed;
+			// console.log(distance);
+			
+			if(direction == 0){
+				if(el.offsetTop > 0.5 * wH)
+					var this_transform = 'translate3d('+x+'px,-'+(y-distance)+'px,0)';
+				else
+					var this_transform = 'translate3d('+x+'px,'+(y+distance)+'px,0)';
+			}
+			else if(direction == 1){
+				if(el.offsetLeft > 0.5 * wW)
+					var this_transform = 'translate3d('+(x-distance)+'px,'+y+'px,0)';
+				else
+					var this_transform = 'translate3d('+(x+distance)+'px,'+y+'px,0)';
+			}
+			else if(direction == 2){
+				if(el.offsetTop < -(0.5 * wH))
+					var this_transform = 'translate3d('+x+'px,'+(y+distance)+'px,0)';
+				else
+					var this_transform = 'translate3d('+x+'px,'+(y-distance)+'px,0)';
+			}
+			else if(direction == 3){
+				if(el.offsetLeft < -(0.5 * wW))
+					var this_transform = 'translate3d('+(x+distance)+'px,'+y+'px,0)';
+				else
+					var this_transform = 'translate3d('+(x-distance)+'px,'+y+'px,0)';
+				
+			}
+		 	el.style.transform = this_transform;
+		});
+		var next_duration = parseInt( (float_maxmin * Math.random() + float_min) * 1000);
+
+		setTimeout(function(){
+			wind(groups, next_duration);
+		}, duration);
+	}
 </script>
