e10abda12af015b376887aa2e1f0bde1c4654c24
Wei-Hao Wang <wwang03@risd.edu>
2021-05-13 20:50:01 +0800

    make images transition

--- views/nav.php
+++ views/nav.php
@@ -76,4 +76,4 @@
 </script>
 <div id="chrome-mask"></div>
 <div id="mask" class="fullpage"><span id="btn-close-mask">close</span></div>
-<div id="mask-loading" class="fullpage"><span id="btn-close-mask">close</span></div>
+<div id="mask-loading" class="fullpage"></div>
